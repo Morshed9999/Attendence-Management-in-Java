b597d01e9385727763c09bc70ecbb8e6849c6c01
Golam Morshed <gm.morshed.99@gmail.com>
2018-05-16 23:09:46 +0600

    Add files via upload

---
+++ Attendanceproj.java
@@ -0,0 +1,36 @@
+package att;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+
+/**
+ *
+ * @author Umang
+ */
+public class Attendanceproj {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+      //  Entryportal f1 = new Entryportal();
+       // f1.setVisible(true);
+
+       new  Addinfoform();
+    new  Addstudent();
+        new   Addteacher();
+        new  Attendanceproj();
+       new  Deletestudent();
+        new  DeleteTeacher();
+        new  Entryportal();
+        new  Report();
+        new  Takeattendence();
+       new    Teacher1();
+    }
+}
